b8e61ee1388a1372f7998801d7bd9be0a70a626a
SergeyKaraman <karamans7997@gmail.com>
2023-04-12 17:41:10 +0300

    Урок 9. Работа с табличными данными
    Урок 9. Работа с табличными данными

--- HW_9/W_1.py
+++ HW_9/W_1.py
@@ -7,8 +7,8 @@ import pandas as pd
 print('Средняя стоимость дома, где кол-во людей от 0 до 500 (population):')
 
 def main():
-    file = pd.read_csv('HW_9\sample_data\california_housing_train.csv')
-    print(file[file['population'] <= 500]['median_house_value'].median())
+    f = pd.read_csv('HW_9\sample_data\california_housing_train.csv')
+    print(f[f['population'] <= 500]['median_house_value'].median())
 
 
 if __name__ == '__main__':
--- HW_9/W_2.py
+++ HW_9/W_2.py
@@ -6,9 +6,8 @@ import pandas as pd
 print('Максимальная households в зоне минимального значения population:')
 
 def main():
-    file = pd.read_csv('HW_9\sample_data\california_housing_train.csv')
-    print(file[file['population'] == file['population'].min()]
-          ['households'].max())
+    f = pd.read_csv('HW_9\sample_data\california_housing_train.csv')
+    print(f[f['population'] == f['population'].min()]['households'].max()
 
 if __name__ == '__main__':
     main()
